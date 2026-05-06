b20c7e23d70d6c38d6ebfea171d0ca34fe255039
PiWizard <ferreirakonstantin@gmail.com>
2026-05-06 14:26:43 -0700

    Update launcher.py

--- core/launcher.py
+++ core/launcher.py
@@ -1,1 +1,15 @@
+import os, sys, subprocess
 
+def launch_new_terminal(script_path):
+    try:
+        if os.name == "nt":
+            subprocess.Popen(f'start "" "{sys.executable}" "{script_path}"', shell=True)
+        elif sys.platform == "darwin":
+            subprocess.Popen([
+                "osascript", "-e",
+                f'tell application "Terminal" to do script "{sys.executable} \\"{script_path}\\""'
+            ])
+        else:
+            subprocess.Popen(["x-terminal-emulator", "-e", f'{sys.executable} "{script_path}"'])
+    except Exception as e:
+        print(f"Launch failed: {e}")
