c4202e566cba401895894937cd81558fd59c50c6
PiWizard <ferreirakonstantin@gmail.com>
2026-05-06 14:30:16 -0700

    Update python_mode.py

--- runtime/python_mode.py
+++ runtime/python_mode.py
@@ -1,1 +1,17 @@
+from utils.colors import *
 
+def python_mode():
+    print(f"{CYAN}Entering Python mode (exit() to leave){RESET}")
+    while True:
+        try:
+            code = input(">>> ")
+            if code == "exit()":
+                break
+            try:
+                result = eval(code)
+                if result is not None:
+                    print(result)
+            except:
+                exec(code)
+        except Exception as e:
+            print(f"{RED}{e}{RESET}")
