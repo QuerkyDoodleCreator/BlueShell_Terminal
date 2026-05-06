81b4da99aea27ffd6493bfd51bee7a20fd3ee084
PiWizard <ferreirakonstantin@gmail.com>
2026-05-06 14:38:45 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 
 VERSION_CHECK_URL = "https://raw.githubusercontent.com/QuerkyDoodleCreator/BlueShell_Terminal/main/version.txt"
 SCRIPT_URL = "https://raw.githubusercontent.com/QuerkyDoodleCreator/BlueShell_Terminal/main/main.py"
